00ff9d3bb7bdb8ef25dc68fbe19a9807099375cf
ravgeetdhillon <ravgeetdhillon@gmail.com>
2022-02-21 13:17:09 +0530

    fix: added reference to circle ci

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Home</h1>
-    <p>This is Home page</p>
+    <p>This is Home page from CircleCI deploy</p>
     <nuxt-link to="/about">Go to About</nuxt-link>
   </div>
 </template>
