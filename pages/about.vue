487e54401de42b388f26e246aad3e680709dd1cc
ravgeetdhillon <ravgeetdhillon@gmail.com>
2022-02-21 13:11:50 +0530

    feat: added circle ci reference

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>About</h1>
-    <p>This is About page</p>
+    <p>This is About page from CircleCI deploy</p>
     <nuxt-link to="/">Go to Home</nuxt-link>
   </div>
 </template>
